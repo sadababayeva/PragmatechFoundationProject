bb8e38092aa2db9563fe6b20d2a8c8edbddf06f2
sadababayeva <sedababayeva7@gmail.com>
2021-07-03 17:36:34 +0400

    js progressing

--- ProjectFrontEnd/js/index.js
+++ ProjectFrontEnd/js/index.js
@@ -52,3 +52,27 @@ function increment3(){
 let interval3=setInterval(increment3,1)
 
 
+let slides=document.getElementsByClassName('customer-feedback')
+let dots = document.getElementsByClassName("dot")
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;   
+    if (n > slides.length) {slideIndex = 1}    
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+}
+  
